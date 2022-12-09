c1b3f3f5e31eec7568b44a610dd64f11eae6d6c9
onlypatric <patric.personal99@gmail.com>
2022-12-09 13:12:35 +0100

    added setup info

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='choiceinput',  
-     version='0.1',
-     scripts=['__init__',"choiceInput"] ,
-     author="Patric Pintu",
-     author_email="patric.personal99@gmail.com",
-     description="choice input in terminal python",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
-     packages=setuptools.find_packages(),
-     classifiers=[
+    name='choiceinput',
+    version='0.1',
+    scripts=['__init__.py', "choiceInput.py"],
+    author="Patric Pintu",
+    author_email="patric.personal99@gmail.com",
+    description="choice input in terminal python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/onlypatric/ChoiceInput",
+    packages=setuptools.find_packages(),
+    classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
-     ],
- )
+    ],
+)
